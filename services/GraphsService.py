715605f0d8892a5a46a8d84b2a03ee5d47fe9712
ReDAndFel <pipecar366@gmail.com>
2024-04-12 10:00:27 -0500

    Se agregó la estructura para leer json y realizar las graficas

---
+++ services/GraphsService.py
@@ -0,0 +1,31 @@
+import services.JsonService as js
+import matplotlib.pyplot as plt
+import numpy as np
+
+json_times_file_path = "../times2.json"
+data = js.read_json(json_times_file_path)
+
+labels = list(data.keys())
+values = list(data.values())
+
+# Asegurarse de que todas las listas tengan el mismo tamaño
+min_length = min(len(labels))
+labels = labels[:min_length]
+values = values[:min_length]
+
+fig, ax = plt.subplots(figsize=(12, 6))
+
+x = np.arange(len(labels))
+width = 0.4
+
+ax.bar(x - width/2, values, width, label='Tiempo')
+
+ax.set_xlabel('Algoritmo')
+ax.set_ylabel('Tiempo')
+ax.set_title('Comparación de tiempos algoritmos de ordenamiento')
+ax.set_xticks(x)
+ax.set_xticklabels(labels, rotation=90)
+ax.legend()
+
+plt.tight_layout()
+plt.show()
